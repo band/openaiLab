19cabd7ce31235040018a7fd93e98048a0cce68a
William L. Anderson <band@acm.org>
2023-04-10 16:47:46 -0500

    cleanup

--- analyze-output.zsh
+++
@@ -1,9 +0,0 @@
-# 2023-03-20
-# analyze OpenAI query output:
-#  - what documents in the repo contain terms in the query output?
-
-for line in "${(@f)"$(<query-output-list.txt)"}"
-{
-echo $line
-rg -l "$line" ~/Documents/myWikis/myMassiveTestWiki
-}
