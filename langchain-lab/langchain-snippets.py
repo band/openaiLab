e584f1533c50c111851a80be47f7a80c139f4461
William L. Anderson <band@acm.org>
2023-05-09 16:02:02 -0500

    include imports in code snippet files

--- langchain-lab/langchain-snippets.py
+++ langchain-lab/langchain-snippets.py
@@ -11,6 +11,7 @@ llm = HuggingFaceHub(repo_id = "google/flan-t5-xl")
 # HuggingFace Local Pipelines
 # pip install transformers
 # need to find the appropriate model_id and model is downloaded into ~.cache
+from langchain import HuggingFacePipeline
 llm = HuggingFacePipeline.from_model_id(model_id="bigscience/bloom-1b7", task="text-generation", model_kwargs={"temperature":0, "max_length":64})
 
 # specify the document loader
