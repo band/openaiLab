26caacf4dca543f92cb165d815ee52ddb052c3be
William L. Anderson <band@acm.org>
2023-05-09 07:52:11 -0500

    using pipeline for local model?

--- langchain-lab/langchain-snippets.py
+++ langchain-lab/langchain-snippets.py
@@ -8,6 +8,10 @@ llm=OpenAI(model_name="text-ada-001", temperature=0))
 from langchain import HuggingFaceHub
 llm = HuggingFaceHub(repo_id = "google/flan-t5-xl")
 
+# HuggingFace Local Pipelines
+# pip install transformers
+# need to find the appropriate model_id and model is downloaded into ~.cache
+llm = HuggingFacePipeline.from_model_id(model_id="bigscience/bloom-1b7", task="text-generation", model_kwargs={"temperature":0, "max_length":64})
 
 # specify the document loader
 from langchain.document_loaders import DirectoryLoader
